9154a57a3a62d448ceb290699293674c62fad5e8
Saurabh Singh <saurabhsinghooo79@gmail.com>
2018-11-07 13:30:36 +0530

    Add files via upload

---
+++ Assg3/script.js
@@ -0,0 +1,20 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason",
+ "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+    function byeSpeaker(names)
+    {
+      console.log("Good Bye " + names);
+    }
+  } else {
+    helloSpeaker(names[i]);
+    function helloSpeaker(names){
+      console.log("Hello " + names);
+    }
+  }
+}}
+)();
